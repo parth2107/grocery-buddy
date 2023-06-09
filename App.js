79e2bcfef0983b3412c6aca566e9bb18a6321e07
Parth Raval <parthraval217@gmail.com>
2023-06-09 07:44:53 -0400

    Initial Commit - Created Initial Screen... Working on HomeScreen UI

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import InitialScreen from '../grocery_buddy/src/screens/InitialScreen/InitialScreen';
 
-export default function App() {
+import { createStackNavigator } from '@react-navigation/stack'; //Insert screens into a stack
+
+const AuthStack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+{/* Add Drawer.Navigation to a function.*/}
+function Root() {
+  return (
+      <Drawer.Navigator initialRouteName="InitialScreen">
+        {/* <Drawer.Screen name="InitialScreen" component={InitialScreen} /> */}
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        {/* <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} /> */}
+      </Drawer.Navigator>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthStack.Navigator>
+      <AuthStack.Screen 
+            name="Initial"
+            component={InitialScreen}
+            options={{
+                headerShown: false,
+            }}
+        />
+      <AuthStack.Screen 
+          name="Root" 
+          component={Root}
+          options={{ headerShown: false }} 
+        />         
+        </AuthStack.Navigator>
+    </NavigationContainer>
+);
+}
---
+++ src/screens/InitialScreen/InitialScreen.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+//for customizing registration button:
+const RegistrationButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.btnRegister}>
+        <Text style={styles.btnText}>{title}</Text>
+    </TouchableOpacity>
+);
+
+//for customizing login button:
+const LoginButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.btnLogin}>
+        <Text style={styles.btnText}>{title}</Text>
+    </TouchableOpacity>
+);
+
+export default function InitialScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageLogo}>
+                <Image
+                    style={{ width: 102, height: 141, marginBottom: 20, }}
+                    // source={require('../../../../assets/Union.png')}
+                />
+                <Image
+                    // source={require('../../../../assets/PassitOn.png')}
+                />
+                <Text style={{ marginTop: 15, color: 'gray' }}>Assets Sharing Platform</Text>
+            </View>
+            {/* <RegistrationButton title="Register" size="sm" onPress={() => navigation.navigate('Registration')}></RegistrationButton> */}
+            <LoginButton title="Start" size="sm" onPress={() => navigation.navigate('Root')}></LoginButton>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    //for main view
+    container: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        justifyContent: 'space-between',
+        // padding:16 ,
+        alignContent: 'center',
+        flexDirection: 'column',
+        paddingHorizontal: 25
+    },
+    //for button
+    btnRegister: {
+        flex: 1,
+        elevation: 19,    //means shadow for btn 
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        backgroundColor: '#21DA8F',
+        borderRadius: 68,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20,
+    },
+    //for button
+    btnLogin: {
+        flex: 1,
+        elevation: 19,    //means shadow for btn 
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        // backgroundColor: '#153759',
+        backgroundColor: '#21DA8F',
+        borderRadius: 68,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 70,
+
+    },
+    //text in btn
+    btnText: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: '400',
+        alignSelf: 'center'
+    },
+
+    imageLogo: {
+        flex: 20,
+        alignItems: "center",
+        justifyContent: 'center',
+    },
+});
