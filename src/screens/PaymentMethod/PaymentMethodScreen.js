0b8e047bdc97cca9b37fcb05ac53c370d1754a6f
Parth Raval <parthraval217@gmail.com>
2023-06-16 09:53:51 -0400

    Made UI realted changes in Payment and Profile Screen...

--- src/screens/PaymentMethod/PaymentMethodScreen.js
+++ src/screens/PaymentMethod/PaymentMethodScreen.js
@@ -1,21 +1,88 @@
 
 //important imports
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native'; // importing components
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'; // importing components
+
+const LocateMeButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={onPress} style={styles.btnSave}>
+    <Text style={styles.btnText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 export default function PaymentMethodScreen() {
     return(
-        <View style={styles.container}>
-        <Text>You are on the Payment Method</Text>
-      </View>
+      
+      <KeyboardAvoidingView
+          style={styles.container}
+          behavior="padding"
+      >
+          <View style={styles.inputContainer}>
+              <View style={{ position: "relative", width: "100%", marginBottom: 8 }}>
+                  <Text>Credit Card</Text>
+                  <TextInput
+                      placeholder='XXXX-XXXX-XXXX-XXXX'
+                      // value={value}
+                      // onChangeText={text => handleChangeTextInput(text, "password")}
+                      // secureTextEntry={secureTextEntry}
+                      style={styles.input}
+                  />
+              </View>
+              <View style={{ position: "relative", width: "100%"}}>
+                  <Text>Expiry</Text>
+                  <TextInput
+                      placeholder='--/--'
+                      // value={value}
+                      // onChangeText={text => handleChangeTextInput(text, "password")}
+                      // secureTextEntry={secureTextEntry}
+                      style={styles.input}
+                  />
+              </View>
+          </View>
+          <LocateMeButton
+              title="Save"
+              size="sm"
+              onPress={() => navigation.navigate("Root")}
+            ></LocateMeButton>
+      </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  //for main view
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  inputContainer: {
+      margin: 20,
+      width: '90%',
+  },
+  input: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    // paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    marginVertical: 10
+  },
+  //for button
+  btnSave: {
+    elevation: 19, //means shadow for btn
+    paddingVertical: 10,
+    paddingHorizontal: 60,
+    backgroundColor: "#153759",
+    borderRadius: 68,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10
+  },
+  //text in btn
+  btnText: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "400",
+    alignSelf: "center",
+  },
+});
--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,21 +1,108 @@
 
 //important imports
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native'; // importing components
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'; // importing components
+import { Avatar} from 'react-native-elements';
+
+const SaveButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={onPress} style={styles.btnSave}>
+    <Text style={styles.btnText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 export default function ProfileScreen() {
     return(
-        <View style={styles.container}>
-        <Text>You are on the Profile</Text>
+      <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+  >
+      <View style={styles.inputContainer}>
+      <View style={{ alignItems: "center", width: "100%", marginBottom: 8 }}>
+              <Avatar
+                size="xlarge"
+                rounded
+                title="Image"
+                avatarStyle={{
+                    borderWidth: 1,
+                    borderColor: 'green',
+                    ...Platform.select({
+                    android: {
+                        borderTopWidth: 0,
+                        borderLeftWidth: 0,
+                        borderRightWidth: 0,
+                        borderBottomWidth: 0,
+                    },
+
+                    }),
+                }}
+            />
+            </View>
+          <View style={{ position: "relative", width: "100%", marginBottom: 8 }}>
+              <Text>Full Name</Text>
+              <TextInput
+                  placeholder=''
+                  // value={value}
+                  // onChangeText={text => handleChangeTextInput(text, "password")}
+                  // secureTextEntry={secureTextEntry}
+                  style={styles.input}
+              />
+          </View>
+          <View style={{ position: "relative", width: "100%"}}>
+              <Text>Last Name</Text>
+              <TextInput
+                  placeholder=''
+                  // value={value}
+                  // onChangeText={text => handleChangeTextInput(text, "password")}
+                  // secureTextEntry={secureTextEntry}
+                  style={styles.input}
+              />
+          </View>
       </View>
+      <SaveButton
+          title="Save"
+          size="sm"
+          onPress={() => navigation.navigate("Root")}
+        ></SaveButton>
+  </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+    //for main view
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  inputContainer: {
+      margin: 20,
+      width: '90%',
+  },
+  input: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    // paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    marginVertical: 10
+  },
+  //for button
+  btnSave: {
+    elevation: 19, //means shadow for btn
+    paddingVertical: 10,
+    paddingHorizontal: 60,
+    backgroundColor: "#153759",
+    borderRadius: 68,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10
+  },
+  //text in btn
+  btnText: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "400",
+    alignSelf: "center",
+  },  
   });
